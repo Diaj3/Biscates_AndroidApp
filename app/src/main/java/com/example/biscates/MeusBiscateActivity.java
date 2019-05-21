08d4c1331d4d82a6ed2f432f1d588c8299207245
João Dias <joaomadias.dac@gmail.com>
2019-05-21 03:01:45 +0100

    Buttons problem
    tentei adicionar butões para permitir a passagem de class para class mas sempre que tento usar a app crasha, o código não aparenta ter problemas, tenho de tentar ver o que é

--- app/src/main/java/com/example/biscates/MeusBiscateActivity.java
+++ app/src/main/java/com/example/biscates/MeusBiscateActivity.java
@@ -2,23 +2,19 @@ package com.example.biscates;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.example.biscates.models.Biscates;
+public class MeusBiscateActivity extends AppCompatActivity {
 
-public class BiscatesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_biscates);
+        setContentView(R.layout.meus_biscates);
         try {this.getSupportActionBar().hide();} catch(NullPointerException e){}
 
         // NavBar
@@ -28,13 +24,13 @@ public class BiscatesActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navigation_profile:
-                        Intent intent = new Intent(BiscatesActivity.this, PerfilActivity.class);
+                        Intent intent = new Intent(MeusBiscateActivity.this, PerfilActivity.class);
                         startActivity(intent);
                         break;
                     case R.id.navigation_dashboard:
                         break;
                     case R.id.navigation_configuration:
-                        Intent intent2 = new Intent(BiscatesActivity.this, ConfigsActivity.class);
+                        Intent intent2 = new Intent(MeusBiscateActivity.this, ConfigsActivity.class);
                         startActivity(intent2);
                         break;
                 }
@@ -42,16 +38,17 @@ public class BiscatesActivity extends AppCompatActivity {
             }
         });
 
-        configureNextButton();
+        configureBackButton();
     }
 
-    private void configureNextButton(){
-        Button nextButton = (Button) findViewById(R.id.nextButton);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+    private void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.nextButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(BiscatesActivity.this, MeusBiscateActivity.class));
+                finish();
             }
         });
     }
+
 }
