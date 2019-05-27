48a5522bed9c37c1b7e58db2a3cbc816225e9f9c
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2019-05-27 23:21:44 +0100

    ok now done

--- app/src/main/java/com/example/biscates/AppIntroActivity.java
+++ app/src/main/java/com/example/biscates/AppIntroActivity.java
@@ -21,26 +21,14 @@ public class AppIntroActivity extends AppIntro {
         super.onCreate(savedInstanceState);
         try {this.getSupportActionBar().hide();} catch(NullPointerException e){}
         //setContentView(R.layout.activity_app_intro);
+        addSlide(AppIntroFragment.newInstance("Procura Biscates", "Podes consultar biscates publicados por outras pessoas.\nProcura um que te interesse e entra em contacto!",
+                R.drawable.lista_biscates, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
 
-        addSlide(AppIntroFragment.newInstance("Biscates", "Não é trabalho. É um Biscate",
-                R.drawable.logo_transparent, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
-
-        addSlide(AppIntroFragment.newInstance("Lista de Biscates", "Podes consultar biscates publicados por outras pessoas.\nProcura um que te interesse e entra em contacto!",
-                R.drawable.lista_biscates, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark)));
-
-        addSlide(AppIntroFragment.newInstance("Adiciona Biscates", "Podes publicar os teus biscate.\nPodes dar-lhe um nome, categoria, preço, entre outros!",
-                R.drawable.add_biscate, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
-
-        addSlide(AppIntroFragment.newInstance("Realiza Biscates", "Encontraste um biscate que te interessa?",
-                R.drawable.details_biscate, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark)));
+        addSlide(AppIntroFragment.newInstance("Publica Biscates", "Podes publicar os teus biscate.\nPodes dar-lhe um nome, categoria, preço, entre outros!",
+                R.drawable.add_biscate, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark)));
 
         addSlide(AppIntroFragment.newInstance("Entra em contacto!", "Entra em contacto com a pessoa e põe mãos à obra!",
                 R.drawable.contact_biscate, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
-
-
-
-
-
         // Note here that we DO NOT use setContentView();
 
         // Add your slide fragments here.
